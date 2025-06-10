e8bb4852ce50c8950bfd5ed50d2bc00549beb87f
txm296 <txm296@proton.me>
2025-06-10 18:21:33 +0200

    Add website editor link to admin dashboard navbar

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -53,6 +53,7 @@ $siteSettings = load_settings();
             <a href="bestellungen.php" class="hover:text-blue-600">Bestellungen</a>
             <a href="insights.php" class="hover:text-blue-600">Insights</a>
             <a href="pages.php" class="hover:text-blue-600">Seiten</a>
+            <a href="customize.php" class="hover:text-blue-600">Website bearbeiten</a>
             <a href="templates.php" class="hover:text-blue-600">Templates</a>
         </nav>
     </header>
